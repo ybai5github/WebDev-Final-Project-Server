fc0654a6464795306b1898a08f52d8a4577c606a
Felix <nampanya.f@northeastern.edu>
2022-04-21 23:53:09 -0400

    edited

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ import usersDao from './database/users/users-dao.js';
 import reviewsController from "./controllers/reviews-controller.js";
 import usersModel from "./database/users/users-model.js";
 
-
-
 mongoose.connect('mongodb+srv://felixyn:drinks@cluster0.mwd5s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
 
 // process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
@@ -150,6 +148,43 @@ app.get('/cartitems', async (req, res) => {
     }
 })
 
+// select cart items by email returns hisotry of items ordered by user
+app.get('/cartitems', async (req, res) => {
+    console.log('global email', globalEmail);
+    /*   const cartEmail = globalEmail;
+      console.log('cart email', cartEmail); */
+
+    /*  usersModel.find({email: cartEmail}, {"cartItems": 1}), function(err, usersModels){
+         if (err) return console.error(err);
+         console.log("usersmodel", usersModels);
+         var merged = [].concat.apply([], usersModels[1].cartItems);
+         console.log('merged array', merged);
+         console.log(usersModels[0].cartItems);
+         res.json(merged);
+     } ; */
+    if (globalEmail == "") {
+        res.status(400).json('please purchase the items before viewing the history');
+        console.log("error viewing the history");
+    } else {
+        var query = { email: globalEmail }
+        db.collection('users').find(query).toArray(function (err, result) {
+            if (err) throw err;
+            console.log('result', result);
+            if (result === undefined || result.length == 0) {
+                res.status(400).json('please purchase the items before viewing the history');
+                console.log("error viewing the history");
+            } else {
+                /* console.log('result [0]. cartItems', result[0].name); */
+                var merged = [].concat.apply([], result[0].cartItems);
+                console.log('merged array', merged);
+                /* console.log('result [0]. cartItems', result[0].cartItems[0]); */
+                res.json(merged);
+            }
+        })
+    }
+})
+
+
 app.post('/register', async (req, res) => {
     const newUser = req.body;
     console.log('new user email', newUser.email);
@@ -158,6 +193,8 @@ app.post('/register', async (req, res) => {
     }
     const hash = bcrypt.hashSync(newUser.password);
     var doc = { hash: hash, email: newUser.email };
+
+
     db.collection('login').insertOne(doc, function (error, response) {
         if (error) {
             console.log('Error occurred while inserting');
@@ -182,131 +219,3 @@ app.post('/register', async (req, res) => {
 app.listen(4000, () => {
     console.log('app is running on 4000')
 })
-
-/* var date = new Date();
-var components = [
-    date.getYear(),
-    date.getMonth(),
-    date.getDate(),
-    date.getHours(),
-    date.getMinutes(),
-    date.getSeconds(),
-    date.getMilliseconds()
-];
- */
-/* var id = components.join(""); */
-
-/* app.post('/register', (req, res) => { handleRegister(req, res, db, bcrypt) }) */
-/* app.post('/imageurl', (req,res) => { handleApi(req,res)})
- */
-
-/* app.post('/register', (req, res) => {
-    const { email, name, password, address, dob } = req.body;
-    database.users.push(
-        {
-            id: '100',
-            name: name,
-            email: email,
-            password: password,
-            address: address,
-            dob: dob,
-            joined: new Date()
-        })
-    res.json(database.users[database.users.length - 1]);
-}) */
-// Load hash from your password DB.
-/* bcrypt.compare("bacon", hash, function (err, res) {
-    // res == true
-});
-bcrypt.compare("veggies", hash, function (err, res) {
-    // res = false
-});
- */
-
-app.post('/register', async (req, res) => {
-    const newUser = req.body;
-    console.log('new user email', newUser.email);
-    if (!newUser.email || !newUser.name || !newUser.address || !newUser.password) {
-        return res.status(400).json('incorrect form submission - message from the server');
-    }
-    const hash = bcrypt.hashSync(newUser.password);
-    var doc = { hash: hash, email: newUser.email };
-
-
-    db.collection('login').insertOne(doc, function (error, response) {
-        if (error) {
-            console.log('Error occurred while inserting');
-            return;
-        } else {
-            console.log('inserted record');
-        }
-    });
-
-    console.log('new user email', newUser.email);
-    console.log('new user', newUser);
-    const insertedUser = await usersDao.register(newUser).catch(err => {
-        console.log('error', err);
-
-        res.status(400).json('user or login duplicates not allowed');
-    });
-    console.log(insertedUser);
-    res.json(insertedUser);
-
-})
-
-
-/* const { email, name, password, address} = req.body;
-if (!email || !name || !password){
-    return res.status(400).json('incorrect form submission)');
-}
-const hash = bcrypt.hashSync(password);
- 
-db.transaction(trx => {
-    trx.insert({
-        hash: hash,
-        email: email,
-    })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-            return trx('users')
-                .returning('*')
-                .insert({
-                    email: loginEmail[0].email,
-                    name: name,
-                    address: address,
-                    joined: new Date()
-                })
-                .then(user => {
-                    res.json(user);
-                })
-        })
-        .then(trx.commit)
-        .catch(trx.rollback)
-})
-    .catch(err => res.status(400).json('unable to register')) */
-
-// app.listen(4000, () => {
-//     console.log('app is running on 4000')
-// })
-
-/*
-/signin -> post = success/fail
-/register -> post = user
-/profile/:userId -> GET = user
-/image -> PUT -> user
-
-*/
-
-/* bcrypt.hash(password, null, null, function (err, hash) {
-       console.log(hash);
-       // Store hash in your password DB.
-   }); */
-/* database.users.push({
-    id: id,
-    name: name,
-    email: email,
-    password: password,
-    entires: 0,
-    joined: new Date()
-}) */
